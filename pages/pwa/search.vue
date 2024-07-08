574e35e157e4f1e0784c5a99b96a125ff822375e
R3AP3R edit <147928812+iotserver24@users.noreply.github.com>
2024-07-08 22:42:09 +0530

    Update search.vue

--- pages/pwa/search.vue
+++ pages/pwa/search.vue
@@ -351,7 +351,7 @@ watch([search, selectGenres, selectTags], () => {
       </v-list>
       <v-card-text v-else>
         <div class="loadingBlock">
-          <h1>No results found!</h1>
+          <h1>No results found!if you g=feel an anime is missing pls <a href:"https://t.me/chatavup"> msg here </a></h1>
         </div>
       </v-card-text>
     </v-card>
